c2e4c73254e6bc1d88ecd3a01e4caccccf27b6e9
Alejandro Mery <amery@opensde.org>
2005-09-05 21:14:08 +0000

    * removed from mnemosyne/build.sh code for autocopying .cache files

--- build.sh
+++ build.sh
@@ -14,19 +14,7 @@
 
 pkgloop_action() {
 	local rc=
-	local desc_M=$( grep -e '^\[M\]' ./package/*/$pkg_name/$pkg_name.desc | cut -d' ' -f2- )
-	local desc_P=$( grep -e '^\[P\]' ./package/*/$pkg_name/$pkg_name.desc | cut -d' ' -f2- )
-
         $cmd_buildpkg ; rc=$?
-
-	admdir="build/${SDECFG_ID}/var/adm"
-
-	if [ $SDECFG_TRG_MNEMOSYNE_COPYCACHE -eq 1 -a -r $admdir/cache/$pkg_name ]; then
-		if grep -q -e '^\[M\] .*Alejandro Mery' -e '^\[P\] O' ./package/*/$pkg_name/$pkg_name.desc; then
-			echo_status "mnemosyne: catching cache file."
-			cp $admdir/cache/$pkg_name ./package/$pkg_tree/$pkg_name/$pkg_name.cache
-		fi
-	fi
 
 	if [ -f ./.pause ]; then
 		echo_status "mnemosyne: pausing building..."
